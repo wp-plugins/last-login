d8afdab288244020f90d2e60fc78b98ee54f7962
dkukral <dkukral@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-22 21:10:51 +0000

    tagging 1.0
    git-svn-id: https://plugins.svn.wordpress.org/last-login/tags/1.0@493600 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ last-login.php
@@ -0,0 +1,119 @@
+<?php
+/*
+Plugin Name: Last Login
+Plugin URI: http://yourdomain.com/
+Description: Records logins and provides a shortcode to display the timestamp. 
+Version: 1.0
+Author: Don Kukral
+Author URI: http://yourdomain.com
+License: GPL
+*/
+
+add_action('wp_login', 'last_login_record_login');
+add_action('wp_logout', 'last_login_record_logout');
+
+add_action('admin_menu', 'last_login_admin_menu');
+
+function last_login_admin_menu() {
+    add_options_page('Last Login', 'Last Login', 'administrator',
+        'last-login', 'last_login_settings_page');
+}
+
+function last_login_record_login($login) {
+    $user = get_userdatabylogin($login);
+    update_user_meta($user->ID, 'last_login_lastlogintime', time());
+}
+
+function last_login_record_logout() {
+    $user = wp_get_current_user();
+    update_user_meta($user->ID, 'last_login_lastlogouttime', time());
+}
+
+function last_login_settings_page() {
+        global $wpdb;
+    ?>
+    <div>
+    <h3 style="padding-top: 10px;">Last Login Options</h3>
+
+    <form method="post" action="options.php">
+    <?php wp_nonce_field('update-options'); ?>
+
+    <table width="710" style="padding-top: 15px;">
+    <tr valign="top">
+    <th width="120" scope="row">Online Timeout</th>
+    <td width="406">
+    <input type="text" name="last_login_online_timeout" value="<?php echo get_option('last_login_online_timeout', 0); ?>"/>
+    </td>
+    </tr>
+    <tr>
+    <td></td>
+    <td>
+    <em>Amount of time (in minutes) to show a logged in user as "online"<br/>
+    Use <strong>0</strong> to disable</em>
+    </td>
+    </tr>
+    <tr>
+    <td></td>
+    <td><br/><input type="submit" value="<?php _e('Save Changes') ?>" /></td>
+    </tr>
+    </table>
+
+    <input type="hidden" name="action" value="update" />
+    <input type="hidden" name="page_options" value="last_login_online_timeout" />
+
+    </form>
+    </div>
+    <?php
+}
+
+function get_last_login_status($user_id) {
+    $lastlogin = get_user_meta($user_id, 'last_login_lastlogintime', true);
+    $lastlogout = get_user_meta($user_id, 'last_login_lastlogouttime', true);
+    $onlinetime = time() - get_option('last_login_online_timeout', 0) * 60;
+    if ($onlinetime) {
+        if (($lastlogin > $onlinetime) && ($lastlogin > $lastlogout)) { 
+            return true;
+        }
+    }
+    return false;
+}
+
+function get_last_login_status_current_user() {
+    $current_user = wp_get_current_user();
+    return get_last_login_status($current_user->ID);
+}
+
+function get_last_login($user_id, $format='%c') {
+    $user = get_userdata($user_id);
+    $lastlogin = get_user_meta($user->ID, 'last_login_lastlogintime', true);
+    if ($lastlogin) {
+        $t = strftime($format, $lastlogin);
+    } else {
+        $t = "";
+    }
+    
+    return $t;
+}
+
+function get_last_login_current_user($format='%c') {
+    $current_user = wp_get_current_user();
+    return last_login_time($current_user->ID, $format);    
+}
+
+function check_last_login($user_id) {
+    $user = get_userdata($user_id);
+    $lastlogin = get_user_meta($user->ID, 'last_login_lastlogintime', true);
+    if ($lastlogin) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function check_last_login_current_user() {
+    $user = wp_get_current_user();
+    return check_last_login($user->ID);
+}
+
+
+?>
---
+++ readme.txt
@@ -0,0 +1,79 @@
+=== Last Login ===
+Contributors: jboydston, dkukral
+Donate link: 
+Tags: login
+Requires at least: 3.0
+Tested up to: 3.3.1
+Stable tag: 1.0
+
+Plugin for WordPress that records the last time a user logged in and provides a function to display that date.
+
+== Description ==
+
+Records logins and provides a function to display the timestamp. 
+
+== License ==
+Copyright 2010 - 2012 Joe Boydston, Don Kukral
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+== Installation ==
+
+Copy "last-login" folder to your WordPress Plugins Directory. 
+Activate plugin via WordPress settings.
+
+== Other Notes ==
+
+None. 
+
+== Screenshots ==
+
+None.
+
+== Changelog ==
+
+= 0.0 = 
+Initial Version
+
+
+== Frequently Asked Questions ==
+
+= How do I display the last login of the current user? =
+
+&lt;php echo get_last_login_current_user(); %gt;
+
+= How do I display the last login of a specific user? =
+
+&lt;php echo get_last_login($user_id); %gt;
+
+= How do I change the date format of the last login time? =
+
+You can pass the format to either last_login_time_current_user() or last_login_time() functions.
+
+Use standard PHP strftime format parameters.
+(http://php.net/manual/en/function.strftime.php)
+
+&lt;php echo get_last_login_current_user($format); %gt;
+&lt;php echo get_last_login($user_id, $format); %gt;
+
+= How can I see if the user has a recorded timestamp? (has logged in since plugin activated) =
+
+You can call check_last_login($user_id) or check_last_login_current_user().
+
+It will return true if a login has been recorded and false if not.
+
+== Upgrade Notice ==
+
+None.
